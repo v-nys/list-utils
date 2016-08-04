12f98e868fff26c02ecac96c99c063235dbfab19
Vincent Nys <vincentnys@gmail.com>
2016-08-04 09:17:02 +0200

    Aangepast zodat ook accumulator beschikbaar is op het einde.

--- utils.rkt
+++ utils.rkt
@@ -1,9 +1,12 @@
 #lang typed/racket
-(: mapAccum (All (ElemType AccType OutType) (-> (-> ElemType AccType (Pair OutType AccType)) AccType (Listof ElemType) (Listof OutType))))
+(: mapAccum (All (ElemType AccType OutType) (-> (-> ElemType AccType (Pair OutType AccType)) AccType (Listof ElemType) (Pair (Listof OutType) AccType))))
+; note: this works, but it is non-tail-recursive
+; will leave it this way for now - if there is a recursion depth problem in an application using this, I'll rewrite it
 (define (mapAccum mapping-function acc lst)
-  (match lst [(list) lst]
+  (match lst [(list) (cons lst acc)]
     [(list-rest h t) (let* ([mapped (mapping-function h acc)]
                             [mapped-val (car mapped)]
-                            [mapped-acc (cdr mapped)])
-                       (cons mapped-val (mapAccum mapping-function mapped-acc t)))]))
+                            [mapped-acc (cdr mapped)]
+                            [rec-call (mapAccum mapping-function mapped-acc t)])
+                       (cons (cons mapped-val (car rec-call)) (cdr rec-call)))]))
 (provide mapAccum)
