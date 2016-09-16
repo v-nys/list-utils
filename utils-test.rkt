eafea8a3a1ee751cf6a829408fc451ca24a3a582
Vincent Nys <vincentnys@gmail.com>
2016-09-16 10:52:40 +0200

    Switching.

--- utils-test.rkt
+++ utils-test.rkt
@@ -1,10 +1,14 @@
 #lang racket
 (require rackunit "utils.rkt")
 (check-equal?
- (mapAccum (λ (e acc) (cons (+ e acc) (- (+ e acc) 1))) 1 '())
+ (map-accumulatel (λ (e acc) (cons (+ e acc) (- (+ e acc) 1))) 1 '())
  (cons '() 1)
  "Sum of element and accumulator, with accumulator = sum - 1, empty list")
 (check-equal?
- (mapAccum (λ (e acc) (cons (+ e acc) (- (+ e acc) 1))) 1 '(1 2 3 4))
+ (map-accumulatel (λ (e acc) (cons (+ e acc) (- (+ e acc) 1))) 1 '(1 2 3 4))
  (cons '(2 3 5 8) 7)
  "Sum of element and accumulator, with accumulator = sum - 1, nonempty list")
+(check-equal?
+ (map-accumulater (λ (e acc) (cons (+ e acc) (- (+ e acc) 1))) 1 '())
+ (cons '() 1)
+ "Sum of element and accumulator, with accumulator = sum - 1, empty list")
--- utils.rkt
+++ utils.rkt
@@ -1,17 +1,19 @@
 #lang racket
 (require racket/contract/parametric)
 ; note: this works, but it is non-tail-recursive
-; will leave it this way for now - if there is a recursion depth problem in an application using this, I'll rewrite it
-(define (mapAccum mapping-function acc lst)
+; will leave it this way for now - if there is a problem in an application using this, I'll rewrite it
+; TODO use right-associative version as a starting point (but consing is more expensive, maybre reverse result at the end?)
+(define (map-accumulatel mapping-function acc lst)
   (match lst [(list) (cons lst acc)]
     [(list-rest h t) (let* ([mapped (mapping-function h acc)]
                             [mapped-val (car mapped)]
                             [mapped-acc (cdr mapped)]
-                            [rec-call (mapAccum mapping-function mapped-acc t)])
+                            [rec-call (map-accumulatel mapping-function mapped-acc t)])
                        (cons (cons mapped-val (car rec-call)) (cdr rec-call)))]))
+
 (provide
  (contract-out
-  [mapAccum
+  [map-accumulatel
    (parametric->/c
     [elem-type? acc-type? mapped-elem-type?]
     (->
@@ -19,3 +21,14 @@
      acc-type?
      (listof elem-type?)
      (cons/c (listof mapped-elem-type?) acc-type?)))]))
+
+
+(define (map-accumulater mapping-function acc lst)
+  (foldr (λ (lst-elem fold-acc)
+           (match fold-acc
+             [(cons mapping map-acc-acc)
+              (let ([mapped-elem (car (mapping-function lst-elem map-acc-acc))]
+                    [updated-map-acc-acc (cdr (mapping-function lst-elem map-acc-acc))])
+                (cons (cons mapped-elem mapping) updated-map-acc-acc))]))))
+              ; TODO add contract!
+(provide map-accumulater)
