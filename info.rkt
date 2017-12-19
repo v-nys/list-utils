d364b7d3e508abc4da31d6e600ee201f76d05217
Vincent Nys <vincentnys@gmail.com>
2017-12-19 08:15:53 +0100

    Version bump.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define scribblings '(("scribblings/list-utils.scrbl")))
 (define collection "list-utils")
-(define version "0.1")
+(define version "0.2")
 (define deps '("at-exp-lib"
                "base"
                "racket-doc"
